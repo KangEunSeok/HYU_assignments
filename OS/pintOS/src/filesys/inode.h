d0039d39d155598f1756c0fe29ce585c94562be4
강은석 <smkoy123@hanyang.ac.kr>
2020-02-09 16:03:47 +0900

    add OS

---
+++ OS/pintOS/src/filesys/inode.h
@@ -0,0 +1,27 @@
+#ifndef FILESYS_INODE_H
+#define FILESYS_INODE_H
+
+#include <stdbool.h>
+#include "filesys/off_t.h"
+#include "devices/block.h"
+
+struct bitmap;
+//static bool get_disk_inode( const struct inode *inode, struct inode_disk *inode_disk);
+//static inline size_t bytes_to_sectors(off_t size);
+
+void inode_init (void);
+bool inode_create (block_sector_t, off_t);
+struct inode *inode_open (block_sector_t);
+struct inode *inode_reopen (struct inode *);
+block_sector_t inode_get_inumber (const struct inode *);
+void inode_close (struct inode *);
+void inode_remove (struct inode *);
+off_t inode_read_at (struct inode *, void *, off_t size, off_t offset);
+off_t inode_write_at (struct inode *, const void *, off_t size, off_t offset);
+void inode_deny_write (struct inode *);
+void inode_allow_write (struct inode *);
+off_t inode_length (const struct inode *);
+//static void free_inode_sectors( struct inode_disk *inode_disk);
+
+
+#endif /* filesys/inode.h */
